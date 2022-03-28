13ec1efcf59be0b5feaca570cc7afeced4dddf9a
kogoome <kogoome@kakao.com>
2022-03-28 18:02:47 +0900

    +

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,8 +1,8 @@
 <nav>
-  <ul class="flex list-none">
-    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/">index</a></li>
-    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/login">login</a></li>
-    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/csr">csr</a></li>
-    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/csr_to_ssr">ssr</a></li>
+  <ul class="flex list-none justify-center">
+    <li class="mr-4 font-bold text-4xl hover:text-[#52a7ec] duration-150"><a href="/">index</a></li>
+    <li class="mr-4 font-bold text-4xl hover:text-[#52a7ec] duration-150"><a href="/login">login</a></li>
+    <li class="mr-4 font-bold text-4xl hover:text-[#52a7ec] duration-150"><a href="/csr">csr</a></li>
+    <li class="mr-4 font-bold text-4xl hover:text-[#52a7ec] duration-150"><a href="/csr_to_ssr">ssr</a></li>
   </ul>
 </nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import Nav from '$lib/nav.svelte';
 	import '../app.css';
+	import {page} from '$app/stores'
+
+	console.log($page.url)
 </script>
 
 <Nav />
