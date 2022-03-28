40e88233e9b0781d3879eff8cdf779aa1d5b6bb6
chanwon moon <kogoome@kakao.com>
2022-03-28 21:02:41 +0900

    네비 메뉴 자동화 모듈

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,3 +1,22 @@
+<script context="module">
+  // 네비 메뉴 자동화
+  const modules = import.meta.glob("../routes/**");
+  const pathArr = Object.keys(modules)
+  const allMenu = pathArr
+    // __는 포함하지 않는 .svelte 파일만 추출, 
+    .filter(path => path.includes(".svelte") && !path.includes("__"))
+    .map(path => {
+      let cleanPath = path.slice(9).replace(".svelte", "")
+      const title = cleanPath.slice(1).includes("/index")?cleanPath.replace("/index",""):cleanPath.slice(1) // /index 있으면 잘라내기
+      const link = cleanPath.includes("/index")?cleanPath=="/index"?cleanPath="/":cleanPath.replace("/index",""):cleanPath.replace("index","")
+      return {
+        title,
+        link
+      }
+    })
+  console.log(allMenu)
+</script>
+
 <script>
 	import {page} from '$app/stores'
 
@@ -15,7 +34,7 @@
 <nav>
   <ul class="flex list-none justify-center gap-6">
     {#each nav as item}
-      <li class="mr-4 font-bold text-4xl hover:text-[#52a7ec] duration-150">
+      <li class="font-bold text-4xl hover:text-[#52a7ec] duration-150">
         <a class:active={path === item.path} href={item.path}>{item.title}</a>
       </li>
     {/each}
