4f25abd40672cbe153d7b37a25589c1221149b5c
chanwon moon <kogoome@kakao.com>
2022-03-28 22:13:28 +0900

    네비 자동화 구성

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,33 +1,14 @@
-<script context="module">
-  // 네비 메뉴 자동화
-  const modules = import.meta.glob("../routes/**");
-  const pathArr = Object.keys(modules)
-  const allMenu = pathArr
-    // __는 포함하지 않는 .svelte 파일만 추출, 
-    .filter(path => path.includes(".svelte") && !path.includes("__"))
-    .map(path => {
-      let cleanPath = path.slice(9).replace(".svelte", "")
-      const title = cleanPath.slice(1).includes("/index")?cleanPath.replace("/index",""):cleanPath.slice(1) // /index 있으면 잘라내기
-      const link = cleanPath.includes("/index")?cleanPath=="/index"?cleanPath="/":cleanPath.replace("/index",""):cleanPath.replace("index","")
-      return {
-        title,
-        link
-      }
-    })
-  console.log(allMenu)
-</script>
-
 <script>
 	import {page} from '$app/stores'
 
-  let path 
-  $: path=$page.url.pathname
+  let link 
+  $: link=$page.url.pathname
 
   const nav = [
-    {title:"index", path:"/"},
-    {title:"login", path:"/login"},
-    {title:"csr", path:"/csr"},
-    {title:"ssr", path:"/ssr"},
+    { title: 'csr', link: '/csr' },    
+    { title: 'index', link: '/' },     
+    { title: 'login', link: '/login' },
+    { title: 'ssr', link: '/ssr' }
   ]
 </script>
 
@@ -35,7 +16,7 @@
   <ul class="flex list-none justify-center gap-6">
     {#each nav as item}
       <li class="font-bold text-4xl hover:text-[#52a7ec] duration-150">
-        <a class:active={path === item.path} href={item.path}>{item.title}</a>
+        <a class:active={link === item.link} href={item.link}>{item.title}</a>
       </li>
     {/each}
   </ul>
