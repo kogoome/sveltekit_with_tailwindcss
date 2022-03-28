de2f4280869340a14ebe60e55cc7916cf3672a7f
chanwon moon <kogoome@kakao.com>
2022-03-28 10:35:28 +0900

    네비 호버 색

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,8 +1,8 @@
 <nav>
   <ul class="flex list-none">
-    <li><a href="/">index</a></li>
-    <li><a href="/login">login</a></li>
-    <li><a href="/csr">csr</a></li>
-    <li><a href="/csr_to_ssr">ssr</a></li>
+    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/">index</a></li>
+    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/login">login</a></li>
+    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/csr">csr</a></li>
+    <li class="mr-4 font-bold hover:text-[#52a7ec] duration-150"><a href="/csr_to_ssr">ssr</a></li>
   </ul>
 </nav>
